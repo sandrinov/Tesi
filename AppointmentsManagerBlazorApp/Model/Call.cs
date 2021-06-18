e9b7d6091ada0e83a3c151f9acea1682785713ad
sandro.vecchiarelli <sandro.vecchiarelli@pomiager.com>
2021-06-18 09:11:46 +0200

    commit 16_06_2021

--- AppointmentsManagerBlazorApp/Model/Call.cs
+++ AppointmentsManagerBlazorApp/Model/Call.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,10 +9,14 @@ namespace AppointmentsManagerBlazorApp.Model
     public class Call
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Mandatory Field Name")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "Mandatory Field Name")]
+        [StringLength(50)]
         public string Provider { get; set; }
         public DateTime Date { get; set; }
         public string Description { get; set; }
+        [StringLength(500)]
         public string Note { get; set; }
     }
 }
